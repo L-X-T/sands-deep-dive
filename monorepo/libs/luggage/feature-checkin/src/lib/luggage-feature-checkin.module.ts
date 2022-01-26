0e62037b293857a09e7dc5e4c193bbf93f5fb9b5
Alex Thalhammer <alex@angular-software.at>
2022-01-26 15:27:26 +0100

    09b_DDD: Bonus: Route Between Feature Libs

--- monorepo/libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ monorepo/libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
-  declarations: [
-    CheckinComponent
-  ],
-  exports: [
-    CheckinComponent
-  ]
+  imports: [CommonModule, LuggageDomainModule, RouterModule.forChild([{ path: '', component: CheckinComponent }])],
+  declarations: [CheckinComponent],
+  exports: [CheckinComponent]
 })
 export class LuggageFeatureCheckinModule {}
--- monorepo/libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
+++ monorepo/libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { ReportLossComponent } from './report-loss.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
-  declarations: [
-    ReportLossComponent
-  ],
-  exports: [
-    ReportLossComponent
-  ]
+  imports: [CommonModule, LuggageDomainModule, RouterModule.forChild([{ path: '', component: ReportLossComponent }])],
+  declarations: [ReportLossComponent],
+  exports: [ReportLossComponent]
 })
 export class LuggageFeatureReportLossModule {}
