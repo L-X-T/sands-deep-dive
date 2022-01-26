5454b0783b4b8b34f681a1d4bc0ee9d5c7009bc8
Alex Thalhammer <alex@angular-software.at>
2022-01-26 15:31:07 +0100

    09b_DDD: Bonus: Implement an UI Library **

--- monorepo/libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
+++ monorepo/libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { ReportLossComponent } from './report-loss.component';
 import { RouterModule } from '@angular/router';
+import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule, RouterModule.forChild([{ path: '', component: ReportLossComponent }])],
+  imports: [CommonModule, LuggageDomainModule, LuggageUiCardModule, RouterModule.forChild([{ path: '', component: ReportLossComponent }])],
   declarations: [ReportLossComponent],
   exports: [ReportLossComponent]
 })
